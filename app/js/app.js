6db5ad4ec91ab89443713659b3f9d0b16bc6141a
Hugo Barbato <hugobarbato@gmail.com>
2016-06-08 23:11:59 +0000

    Help Us 2

--- app/js/app.js
+++ app/js/app.js
@@ -43,27 +43,8 @@
 })
 
     // Controllers
-    	.controller("loginCtrl", function($scope, $auth){
-    		
-	    	$scope.logar = function(provider){
-	    		var user = $scope.user;
-	    		$auth.login(user)
-				  .then(function(response) {
-				    // Redirect user here after a successful log in.
-				  })
-				  .catch(function(response) {
-				    // Handle errors here, such as displaying a notification
-				    // for invalid email and/or password.
-				  });
-	    	};
-	    	
-	    	$scope.authenticate = function(provider) {
-		      $auth.authenticate(provider);
-		    };
     	
-    	})
-    	
-    	
+    	//CADASTRO DOADOR
     	.controller("cadastroCtrl", function($scope){
     		$scope.showHints = true;
 			 $scope.user = {
